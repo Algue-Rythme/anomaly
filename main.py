72f19f32c08760f3ee1221222addb33fc0d212cc
Algue-Rythme <algue-rythme@orange.fr>
2020-11-12 14:37:20 +0100

    reverse remote matplotlib

--- main.py
+++ main.py
@@ -60,13 +60,15 @@ def renormalize_grads(grads):
     return [renormalize(grad) for grad in grads]
 
 
-def generate_adversarial(model, x_0, margin, phase):
-    max_iter = 10
-    lr       = 5.
-    h_x_0    = 2.
-    h_x      = 0.
+def generate_adversarial(model, x_0, scale, phase):
+    return scale * tf.random.uniform(x_0.shape, [[-2., -1.5]], [[3., 2.]])
 
-    learning_rate = lr * (margin / max_iter)
+    max_iter = 10
+    lr       = 2.
+    h_x_0    = 2.
+    h_x      = 1.
+
+    learning_rate = lr * (scale / max_iter)
     optimizer     = SGD(learning_rate=learning_rate)
 
     # x_init is perturbed x_0, with atmost 10% of a gradient step (which can be admittely quite high)
@@ -75,8 +77,8 @@ def generate_adversarial(model, x_0, margin, phase):
     for _ in range(max_iter):
         with tf.GradientTape() as tape:
             y = model(x)
-            dispersion   = h_x   * metric_entropy(x, x, margin)
-            fidelity     = h_x_0 * metric_entropy(x, x_0, margin)
+            dispersion   = h_x   * metric_entropy(x, x, scale)
+            fidelity     = h_x_0 * metric_entropy(x, x_0, scale)
             loss = tf.reduce_mean(y) + dispersion + fidelity
             loss = -loss  # minimize -loss <=> maximize loss (but must be regularized)
         grad_f_x = tape.gradient(loss, [x])
@@ -85,8 +87,8 @@ def generate_adversarial(model, x_0, margin, phase):
     return x.value()
 
 
-def one_class_wasserstein(model, x, optimizer, lbda, alpha, margin, phase):
-    adv = generate_adversarial(model, x, margin, phase)
+def one_class_wasserstein(model, x, optimizer, lbda, alpha, scale, phase):
+    adv = generate_adversarial(model, x, scale, phase)
 
     with tf.GradientTape(persistent=True) as tape:
         # tape.watch(x)
@@ -95,8 +97,8 @@ def one_class_wasserstein(model, x, optimizer, lbda, alpha, margin, phase):
         y_adv = model(adv)
 
         wasserstein = tf.reduce_mean(y) - tf.reduce_mean(y_adv)
-        # hinge       = oneclass_hinge(margin, y) + oneclass_hinge(margin, -y_adv)
-        hinge       = oneclass_hinge(1., y) #  + oneclass_hinge(margin, -y_adv)
+        # hinge       = oneclass_hinge(1., y) + oneclass_hinge(1., -y_adv)
+        hinge       = oneclass_hinge(1., y) #  + oneclass_hinge(1., -y_adv)
         loss        = -wasserstein + lbda * hinge
 
     _, norm_nabla_f_x = get_grad_norm_with_tape(tape, y, x)
@@ -105,13 +107,13 @@ def one_class_wasserstein(model, x, optimizer, lbda, alpha, margin, phase):
     return loss, wasserstein, hinge, norm_nabla_f_x
 
 
-def train_OOD_detector(model, dataset, num_batchs, lbda, alpha, margin, phase):
+def train_OOD_detector(model, dataset, num_batchs, lbda, alpha, scale, phase):
     optimizer = Adam()
     progress = tqdm(total=num_batchs, ascii=True)
     loss_avg, w_avg, hinge_avg, m = None, None, None, 0.97
     penalties = []
     for _, (x, _) in enumerate(dataset):
-        loss, w, hinge, penalty = one_class_wasserstein(model, x, optimizer, lbda, alpha, margin, phase)
+        loss, w, hinge, penalty = one_class_wasserstein(model, x, optimizer, lbda, alpha, scale, phase)
         loss_avg = exp_avg(loss_avg, loss, m); w_avg = exp_avg(w_avg, w, m); hinge_avg = exp_avg(hinge_avg, hinge, m)
         penalties.append(penalty)
         desc = f'Loss={float(loss_avg):>2.3f} Wasserstein={float(w_avg):>2.3f} Hinge={float(hinge_avg):>2.3f}'
@@ -148,22 +150,21 @@ def plot_levels_lines(sk_func):
     lbda       = 1.
     alpha      = 0.
     phase      = 'adversarial'
-    margin     = 1.  #5.
-    dilatation = 1.  # 10.
-    sk_func    = dilated_func(sk_func, dilatation)
+    scale      = 10.
+    sk_func    = dilated_func(sk_func, scale)
 
     dataset    = tf_dataset(num_batchs, batch_size, sk_func)
 
-    X, Y       = sk_func(100, noise=0.1)
+    X, Y       = sk_func(1000, noise=0.1)
     num_examples_draw = 1000
     fig        = plt.figure(figsize=(20,14))
     plt.subplot(2, 3, 1)
     plotex.plot_levels(X, Y, model)
     plt.subplot(2, 3, 2)
-    draw_adv(model, sk_func, num_examples_draw, margin, phase)
+    draw_adv(model, sk_func, num_examples_draw, scale, phase)
 
     plt.subplot(2, 3, 3)
-    penalties = train_OOD_detector(model, dataset, num_batchs, lbda, alpha, margin, phase)
+    penalties = train_OOD_detector(model, dataset, num_batchs, lbda, alpha, scale, phase)
     iterations = np.arange(len(penalties))
     plt.plot(iterations, np.log10(tf.reduce_mean(penalties, axis=1).numpy()))
     plt.plot(iterations, np.log10(tf.reduce_min(penalties, axis=1).numpy()))
@@ -173,7 +174,7 @@ def plot_levels_lines(sk_func):
     plt.subplot(2, 3, 4)
     plotex.plot_levels(X, Y, model)
     plt.subplot(2, 3, 5)
-    draw_adv(model, sk_func, num_examples_draw, margin, phase)
+    draw_adv(model, sk_func, num_examples_draw, scale, phase)
 
     plt.show()
 
